f45e51892f7f9422b7f6c011471ac0272613ec46
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2026-08-03 22:29:09 +0800

    style: apply pint formatting and add dev dependencies
    Run Laravel Pint to normalize code style (use ordering, spacing,
    type hints) and add pint/phpstan as dev dependencies.

--- src/Contracts/BingWallpaperInterface.php
+++ src/Contracts/BingWallpaperInterface.php
@@ -4,17 +4,16 @@ namespace Haoyuqi\DownloadBingWallpaper\Contracts;
 
 interface BingWallpaperInterface
 {
-
     /**
      * @return string
      */
     public function download();
 
     /**
-     * @param string $content
-     * @param string $save_path
-     * @param string $file_name
-     * @return boolean
+     * @param  string  $content
+     * @param  string  $save_path
+     * @param  string  $file_name
+     * @return bool
      */
     public function save($content, $save_path, $file_name = null);
 }
